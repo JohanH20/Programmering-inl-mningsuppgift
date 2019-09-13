b22b33db378daba12b24afa3b61ef9aa0ec96b34
Elev <Elev@TEB127031>
2019-09-13 08:29:19 +0200

    Kod

--- Inlämingsuppgift/Inlämingsuppgift/Program.cs
+++ Inlämingsuppgift/Inlämingsuppgift/Program.cs
@@ -6,7 +6,22 @@ namespace Inlämingsuppgift
     {
         static void Main(string[] args)
         {
+            Random tal = new Random();
+            int tal = tal.Random(100);
 
+            bool Vinna = false;
+            while(Vinna == false){
+            Console.Write("Gissa på ett tal mellan 1 och 100: ");
+            int gissning = int.Parse(Console.ReadLine());
+            
+            if(gissning==tal){
+                Vinna=true;
+                Console.WriteLine("Rätt");
+            }
+                else if()
+
+            }
         }
     }
 }
+
