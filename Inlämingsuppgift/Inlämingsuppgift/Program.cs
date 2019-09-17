63fbe768cd3ff8bba99089906113ddf1cd113bcf
Elev <Elev@TEB127027>
2019-09-17 13:21:42 +0200

    Har lagt till att den skriver antal gissningar

--- Inlämingsuppgift/Inlämingsuppgift/Program.cs
+++ Inlämingsuppgift/Inlämingsuppgift/Program.cs
@@ -9,14 +9,17 @@ namespace Inlämingsuppgift
             Random Tal = new Random();
             int tal = Tal.Next(100);
 
+            int gissningar = 0;
             bool Vinna = false;
             while(Vinna == false){
             Console.Write("Gissa på ett tal mellan 1 och 100: ");
             int gissning = int.Parse(Console.ReadLine());
+                gissningar++;
             
             if(gissning==tal){
                 Vinna=true;
                 Console.WriteLine("Rätt");
+                Console.WriteLine("Antal gissningar är:" + gissningar);
             }
                 else if(gissning>tal){
                     Console.WriteLine("Gissningen är för hög");
